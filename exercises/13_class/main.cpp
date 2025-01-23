eb5636ba5ae9ba37f2335af79f40028c08dc6c94
Penguin-SAMA <496107146@qq.com>
2025-01-23 14:19:58 +0800

    learn 13

--- exercises/13_class/main.cpp
+++ exercises/13_class/main.cpp
@@ -8,24 +8,29 @@
 // 这个 class 中的字段被 private 修饰，只能在 class 内部访问。
 // 因此必须提供构造器来初始化字段。
 // READ: 构造器 <https://zh.cppreference.com/w/cpp/language/constructor>
-class Fibonacci {
+class Fibonacci
+{
     size_t cache[16];
     int cached;
 
 public:
     // TODO: 实现构造器
-    // Fibonacci()
+    Fibonacci() {
+        cache[0] = 0;
+        cache[1] = 1;
+        cached = 2;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (cached = 2; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
     }
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     // 现在类型拥有无参构造器，声明时会直接调用。
     // 这个写法不再是未定义行为了。
     Fibonacci fib;
