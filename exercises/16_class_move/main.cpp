58920cf9c49be9924d9aea9d947ae08d24d482f2
Penguin-SAMA <496107146@qq.com>
2025-01-24 03:14:36 +0800

    learn 16

--- exercises/16_class_move/main.cpp
+++ exercises/16_class_move/main.cpp
@@ -1,4 +1,5 @@
 #include "../exercise.h"
+#include <cstddef>
 
 // READ: 左值右值（概念）<https://learn.microsoft.com/zh-cn/cpp/c-language/l-value-and-r-value-expressions?view=msvc-170>
 // READ: 左值右值（细节）<https://zh.cppreference.com/w/cpp/language/value_category>
@@ -9,27 +10,49 @@
 // READ: 移动赋值 <https://zh.cppreference.com/w/cpp/language/move_assignment>
 // READ: 运算符重载 <https://zh.cppreference.com/w/cpp/language/operators>
 
-class DynFibonacci {
-    size_t *cache;
+class DynFibonacci
+{
+    size_t* cache;
     int cached;
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity)
+        : cache(new size_t[capacity])
+        , cached(2) {
+        cache[0] = 0;
+        cache[1] = 1;
+    }
 
     // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci &&) noexcept = delete;
+    DynFibonacci(DynFibonacci&& other) noexcept
+        : cache(other.cache)
+        , cached(other.cached) {
+        other.cache = nullptr;
+        other.cached = 0;
+    }
 
     // TODO: 实现移动赋值
     // NOTICE: ⚠ 注意移动到自身问题 ⚠
-    DynFibonacci &operator=(DynFibonacci &&) noexcept = delete;
+    DynFibonacci& operator=(DynFibonacci&& other) noexcept {
+        if (this != &other) {
+            delete[] cache;
+            cache = other.cache;
+            cached = other.cached;
+            other.cache = nullptr;
+            other.cached = 0;
+        }
+        return *this;
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() {
+        delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t operator[](int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
@@ -47,7 +70,7 @@ public:
     }
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     DynFibonacci fib(12);
     ASSERT(fib[10] == 55, "fibonacci(10) should be 55");
 
