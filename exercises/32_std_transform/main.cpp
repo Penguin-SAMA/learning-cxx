9cd74b750f040a5d94b542da5f72524b3d18f7d3
Penguin-SAMA <496107146@qq.com>
2025-01-25 15:33:54 +0800

    learn 32

--- exercises/32_std_transform/main.cpp
+++ exercises/32_std_transform/main.cpp
@@ -1,15 +1,19 @@
 ﻿#include "../exercise.h"
 #include <algorithm>
+#include <functional>
 #include <string>
 #include <vector>
 
 // READ: `std::transform` <https://zh.cppreference.com/w/cpp/algorithm/transform>
 // READ: `std::vector::begin` <https://zh.cppreference.com/w/cpp/container/vector/begin>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     std::vector<int> val{8, 13, 21, 34, 55};
     // TODO: 调用 `std::transform`，将 `v` 中的每个元素乘以 2，并转换为字符串，存入 `ans`
-    // std::vector<std::string> ans
+    std::vector<std::string> ans{5};
+    std::transform(val.cbegin(), val.cend(), val.begin(), [](int x) { return x * 2; });
+    std::transform(val.cbegin(), val.cend(), ans.begin(), [](int x) { return std::to_string(x); });
+
     ASSERT(ans.size() == val.size(), "ans size should be equal to val size");
     ASSERT(ans[0] == "16", "ans[0] should be 16");
     ASSERT(ans[1] == "26", "ans[1] should be 26");
