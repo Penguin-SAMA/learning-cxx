9c5387fb41b2d580cb60ec3c2a5d2110b3359949
Penguin-SAMA <496107146@qq.com>
2025-01-25 14:49:50 +0800

    learn 27

--- exercises/27_strides/main.cpp
+++ exercises/27_strides/main.cpp
@@ -13,16 +13,24 @@ using udim = unsigned int;
 /// @brief 计算连续存储张量的步长
 /// @param shape 张量的形状
 /// @return 张量每维度的访问步长
-std::vector<udim> strides(std::vector<udim> const &shape) {
+std::vector<udim> strides(std::vector<udim> const& shape) {
     std::vector<udim> strides(shape.size());
     // TODO: 完成函数体，根据张量形状计算张量连续存储时的步长。
     // READ: 逆向迭代器 std::vector::rbegin <https://zh.cppreference.com/w/cpp/container/vector/rbegin>
     //       使用逆向迭代器可能可以简化代码
+
+    udim product = 1;
+    auto s_it = shape.rbegin();
+    auto d_it = strides.rbegin();
+    for (; s_it != shape.rend(); ++s_it, ++d_it) {
+        *d_it = product;
+        product *= *s_it;
+    }
     return strides;
 }
 
 // ---- 不要修改以下代码 ----
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     ASSERT((strides({2, 3, 4}) == std::vector<udim>{12, 4, 1}), "Make this assertion pass.");
     ASSERT((strides({3, 4, 5}) == std::vector<udim>{20, 5, 1}), "Make this assertion pass.");
     ASSERT((strides({1, 3, 224, 224}) == std::vector<udim>{150528, 50176, 224, 1}), "Make this assertion pass.");
