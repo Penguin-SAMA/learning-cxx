5a21f0d140ad75b37a4b6a4b10a040004e3ca256
Penguin-SAMA <496107146@qq.com>
2025-01-25 15:40:40 +0800

    learn 33

--- exercises/33_std_accumulate/main.cpp
+++ exercises/33_std_accumulate/main.cpp
@@ -1,9 +1,11 @@
 ﻿#include "../exercise.h"
+#include <functional>
+#include <iterator>
 #include <numeric>
 
 // READ: `std::accumulate` <https://zh.cppreference.com/w/cpp/algorithm/accumulate>
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     using DataType = float;
     int shape[]{1, 3, 224, 224};
     // TODO: 调用 `std::accumulate` 计算：
@@ -11,7 +13,7 @@ int main(int argc, char **argv) {
     //       - 形状为 shape；
     //       - 连续存储；
     //       的张量占用的字节数
-    // int size =
+    int size = std::accumulate(std::begin(shape), std::end(shape), 1, std::multiplies<>()) * sizeof(DataType);
     ASSERT(size == 602112, "4x1x3x224x224 = 602112");
     return 0;
 }
